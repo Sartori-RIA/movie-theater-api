84ee07eb6701d4db11052892bedaab9d83de6028
Lucas Antonio Ramos Sartori <sartori@letsgrow.com.br>
2020-10-04 22:08:43 -0300

    added rspec config

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:email) { |n| "user#{n}@email.com" }
+    sequence(:cell_phone) { |n| "#{Faker::PhoneNumber.cell_phone}#{n}" }
+    password { '123123123' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.validations' do
+    context 'with be has presence' do
+      it { is_expected.to validate_presence_of(:email) }
+      it { is_expected.to validate_presence_of(:cell_phone) }
+      it { is_expected.to validate_presence_of(:password) }
+    end
+
+    context 'with be unique' do
+      it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+      it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+      it { is_expected.to validate_uniqueness_of(:cell_phone).ignoring_case_sensitivity }
+    end
+  end
 end
